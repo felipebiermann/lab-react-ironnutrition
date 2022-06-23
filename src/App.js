028222da357b52800caca219db0402370bf97428
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-06-23 18:30:36 -0300

    ate iteração 3.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import foods from './foods.json';
+import { useState } from 'react';
+import { FoodBox } from './components/FoodBox';
 
 function App() {
+  const [allFoods, setAllFoods] = useState(foods);
+  console.log(foods);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {foods.map((element) => {
+        return <FoodBox food={element} />;
+      })}
     </div>
   );
 }
